c6984510e15d4ceab6976776c6447f12cb5c930e
Hannah Falk <hannahcfalk@gmail.com>
2022-06-20 12:09:32 +0100

    Initial React cover page

--- near-walking-tours/src/App.js
+++ near-walking-tours/src/App.js
@@ -1,28 +1,48 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useCallback, useState } from "react";
+import { Container, Nav } from "react-bootstrap";
+import { login, logout as destroy, accountBalance } from "./utils/near";
+import Wallet from "./components/Wallet";
+// import { Notification } from "./components/utils/Notifications";
+// import Tours from "./components/tours/Tour";
+import Cover from "./components/utils/Cover";
+import coverImg from "./assets/img/path.jpg";
 import "./App.css";
-import { getTours } from "./utils/tours";
-import { login } from "./utils/near";
 
-function App() {
+const App = function AppWrapper() {
     const account = window.walletConnection.account();
-    const [tours, setTours] = useState([]);
-    const fetchTours = useCallback(async () => {
+    const [balance, setBalance] = useState("0");
+    const getBalance = useCallback(async () => {
         if (account.accountId) {
-            setTours(await getTours());
+            setBalance(await accountBalance());
         }
     });
+
     useEffect(() => {
-        fetchTours();
-    }, []);
+        getBalance();
+    }, [getBalance]);
+
     return (
         <>
+            {/* <Notification /> */}
             {account.accountId ? (
-                <p>hi</p>
+                <Container fluid="md">
+                    <Nav className="justify-content-end pt-3 pb-5">
+                        <Nav.Item>
+                            <Wallet
+                                address={account.accountId}
+                                amount={balance}
+                                symbol="NEAR"
+                                destroy={destroy}
+                            />
+                        </Nav.Item>
+                    </Nav>
+                    <main>{/* <Tours /> */}</main>
+                </Container>
             ) : (
-                <button onClick={login}>CONNECT WALLET</button>
+                <Cover name="NEAR Walking Tours" login={login} coverImg={coverImg} />
             )}
         </>
     );
-}
+};
 
 export default App;
